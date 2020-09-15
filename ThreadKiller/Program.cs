ed3dafb9d6c10fae71e8ac68c3c712cb27a8ce1e
Mehmet ERDOĞDU <mehmet.erdogdu52@hotmail.com>
2020-09-15 12:30:18 +0300

    Add project files.

---
+++ ThreadKiller/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ThreadKiller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var thread = new InterruptExample();
+            int count = 0;
+            while (true)
+            {
+                Process currentProcess = Process.GetCurrentProcess();
+                Console.WriteLine("process count: " + currentProcess.Threads.Count);
+
+
+                count++;
+                Console.WriteLine("Main Thread Running: " + count);
+                Thread.Sleep(250);
+                if (count % 5 == 0)
+                {
+                    thread.Kill(5);
+                    thread = new InterruptExample();
+                }
+            }
+        }
+
+        public class InterruptExample
+        {
+            public Thread job;
+
+            public InterruptExample()
+            {
+                job = new Thread(() =>
+                {
+                    Do();
+                });
+                job.Start();
+            }
+
+            public void Do()
+            {
+                try
+                {
+                    var count = 0;
+                    while (true)
+                    {
+                        Thread.Sleep(250);
+                        Console.WriteLine("FIRST THREAD running... " + count++);
+                    }
+                }
+                catch (ThreadInterruptedException)
+                {
+                    /* Clean up. */
+                }
+            }
+
+            public void Kill(int timeout = 0)
+            {
+                job.Interrupt();
+                job.Join(timeout);
+            }
+        }
+
+    }
+}
